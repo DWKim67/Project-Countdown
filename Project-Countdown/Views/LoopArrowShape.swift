13e482ed747285f3da28a99fa1cb7b2311d14cca
DWKim67 <dkim67@my.yorku.ca>
2025-02-06 14:14:58 -0500

    Refactoring loop arrow coordinates

--- Project-Countdown/Views/LoopArrowShape.swift
+++ Project-Countdown/Views/LoopArrowShape.swift
@@ -10,18 +10,21 @@ import SwiftUI
 struct LoopArrowShape: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
+        let startingPoint = CGPoint(x: 10, y: 200)
+        let sizeMultiplier: CGFloat = 40
         
-        path.move(to: CGPoint(x: rect.width * 0.1 , y: rect.height * 0.3))
-        path.addLine(to: CGPoint(x: rect.width * 0.3, y: rect.height * 0.3))
-        path.addLine(to: CGPoint(x: rect.width * 0.2, y: rect.height * 0.4))
-        path.move(to: CGPoint(x: rect.width * 0.15 , y: rect.height * 0.3))
-        path.addArc(tangent1End: CGPoint(x: rect.width * 0.15 , y: rect.height * 0.2), tangent2End: CGPoint(x: rect.width * 0.3, y: rect.height * 0.2), radius: 50)
-        path.addLine(to: CGPoint(x: rect.width * 0.6, y: rect.height * 0.2))
-        path.addArc(tangent1End: CGPoint(x: rect.width * 0.7 , y: rect.height * 0.2), tangent2End: CGPoint(x: rect.width * 0.65, y: rect.height * 0.3), radius: 50)
-        path.addLine(to: CGPoint(x: rect.width * 0.5, y: rect.height * 0.3))
-//        path.addArc(tangent1End: CGPoint(x: rect.width * 0.7 , y: rect.height * 0.2), tangent2End: CGPoint(x: rect.width * 0.65, y: rect.height * 0.3), radius: 50)
-        
-        //        path.addArc(center: CGPoint(x: rect.width * 0.2, y: rect.height * 0.3), radius: 60, startAngle: .degrees(0), endAngle: .degrees(90), clockwise: true)
+        path.move(to: startingPoint)
+        path.addLine(to: CGPoint(x: startingPoint.x + (2 * sizeMultiplier), y: startingPoint.y))
+        path.addLine(to: CGPoint(x: startingPoint.x + (1 * sizeMultiplier), y: startingPoint.y + (1 * sizeMultiplier)))
+        path.move(to: CGPoint(x: startingPoint.x + (0.5 * sizeMultiplier), y: startingPoint.y))
+        path.addArc(tangent1End: CGPoint(x: startingPoint.x + (0.5 * sizeMultiplier), y: startingPoint.y - (1 * sizeMultiplier)), tangent2End: CGPoint(x: startingPoint.x + (20 * sizeMultiplier), y: startingPoint.y - (1 * sizeMultiplier)), radius: (1 * sizeMultiplier))
+        path.addLine(to: CGPoint(x: startingPoint.x + (0.5 * sizeMultiplier), y: startingPoint.y - (1 * sizeMultiplier)))
+        path.addArc(tangent1End: CGPoint(x: startingPoint.x + (7 * sizeMultiplier) , y: startingPoint.y - (1 * sizeMultiplier)), tangent2End: CGPoint(x: startingPoint.x + (7 * sizeMultiplier), y: startingPoint.y), radius: (1 * sizeMultiplier))
+        path.addLine(to: CGPoint(x: startingPoint.x + (6 * sizeMultiplier), y: startingPoint.y))
+        path.addArc(tangent1End: CGPoint(x: startingPoint.x + (5 * sizeMultiplier), y: startingPoint.y - (0.5 * sizeMultiplier)), tangent2End: CGPoint(x: startingPoint.x + (4.5 * sizeMultiplier), y: startingPoint.y - (0.5 * sizeMultiplier)), radius: (4 * sizeMultiplier))
+        path.addLine(to: CGPoint(x: startingPoint.x + (3.5 * sizeMultiplier), y: startingPoint.y - (0.5 * sizeMultiplier)))
+        path.addLine(to: CGPoint(x: startingPoint.x + (2.5 * sizeMultiplier), y: startingPoint.y - (0.5 * sizeMultiplier)))
+        path.addArc(tangent1End: CGPoint(x: startingPoint.x + (1.5 * sizeMultiplier) , y: startingPoint.y), tangent2End: CGPoint(x: startingPoint.x + (1.5 * sizeMultiplier), y: startingPoint.y), radius: (1 * sizeMultiplier))
         return path
     }
     
